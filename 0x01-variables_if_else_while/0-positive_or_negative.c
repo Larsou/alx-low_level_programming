fe02c2a8565b35fd7a6ae0243438765aa6435027
Larsou <soussi123lrb#gmail.com>
2023-03-17 18:24:26 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - this program determines the input polarity
+ * Return: 0 (Success)
+*/
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n == 0)
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n == 0)
 {
 printf("%d is zero\n", n);
 }
@@ -25,5 +25,5 @@ else
 printf("%d is negative\n", n);
 }
 }
-	return (0);
+return (0);
 }
