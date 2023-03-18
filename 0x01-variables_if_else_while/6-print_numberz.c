2173422eddf601d6a95da47de1b036b60ee85f8e
Larsou <soussi123lrb#gmail.com>
2023-03-17 18:48:35 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,6 +2,7 @@
 /**
  * main - this program prints numbers from 0 to 9.
  *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,4 +13,5 @@ putchar(i);
 }
 putchar('\n');
 return (0);
+}
 }
