cb322e00694ac5497f367c9541e437cf06c3930e
Larsou <soussi123lrb#gmail.com>
2023-04-06 19:04:06 -0700

    6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_prime_number - checks if a number is prime
+ * emyy st yle
+ * @n: the number to check
+ * Return: 1 if n is prime, otherwise 0
+ */
+int is_help_prime(int n, int i);
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	if (n % 2 == 0)
+		return (0);
+
+	return (is_help_prime(n, 3));
+}
+
+/**
+ * is_help_prime - recursive supporter function for is_prime_number()
+ * @n: the integer to check
+ * @divisor: the divisor to test
+ * emmy st yle
+ * Return: 1 if n is prime, otherwise 0
+ */
+
+int is_help_prime(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+
+	return (is_help_prime(n, i + 2));
+}
